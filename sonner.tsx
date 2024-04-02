0394fc212d2837198226e5874467f8c9c4299fa4
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-02 11:48:19 -0400

    feat: add sonner toaster

---
+++ sonner.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import type { ComponentProps } from "react";
+// @ts-ignore -- not sure why this is not working
+import { Toaster as Sonner } from "sonner";
+import cn from "./cn";
+
+type ToasterProps = ComponentProps<typeof Sonner>;
+
+export default function Toaster({ ...props }: ToasterProps) {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group border-"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:border-1 group-[.toaster]:shadow-sm",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      cn={cn}
+      {...props}
+    />
+  );
+}
