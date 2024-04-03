fdbf726f5b4616bf04567c8ab87561b1eccd6ba7
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-03 19:11:22 -0400

    feat: add truncate utils

---
+++ utils/truncate.ts
@@ -0,0 +1,22 @@
+const MAX_LAST_CHUNK_LENGTH = 4;
+
+const truncate = (text: string, chars = 9): string => {
+  if (chars >= text.length) {
+    return text;
+  }
+
+  if (chars < MAX_LAST_CHUNK_LENGTH * 2) {
+    const firstChunk = text.slice(0, Math.max(0, Math.round(chars / 2)));
+    const lastChunk = text.slice(-Math.floor(chars / 2));
+
+    return `${firstChunk}…${lastChunk}`;
+  }
+
+  const firstChunk = text.slice(0, Math.max(0, chars - MAX_LAST_CHUNK_LENGTH));
+
+  const lastChunk = text.substr(text.length - MAX_LAST_CHUNK_LENGTH, chars);
+
+  return `${firstChunk}…${lastChunk}`;
+};
+
+export default truncate;
