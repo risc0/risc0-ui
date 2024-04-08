7c075cdbf8383d15a3c86707344964f8b6bd5529
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-08 13:56:53 -0400

    feat: tooltip arrow

--- tooltip.tsx
+++ tooltip.tsx
@@ -6,17 +6,13 @@ import { cn } from "./cn";
 
 const TooltipContent = forwardRef<
   ElementRef<typeof TooltipPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof TooltipPrimitive.Content> & {
-    // any other prop goes here
-    isInfo?: boolean;
-  }
->(({ className, sideOffset = 4, isInfo = false, ...rest }, ref) => (
+  ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...rest }, ref) => (
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 animate-in overflow-hidden rounded-md bg-primary px-3 py-1.5 text-primary-foreground text-xs data-[state=closed]:animate-out",
-      isInfo && "cursor-help",
+      "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 animate-in overflow-hidden rounded-md bg-primary px-3 py-1.5 text-primary-foreground text-xs data-[state=closed]:animate-out dark:bg-white",
       className,
     )}
     {...rest}
@@ -26,7 +22,8 @@ const TooltipContent = forwardRef<
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
 const TooltipProvider = TooltipPrimitive.Provider;
+const TooltipArrow = TooltipPrimitive.Arrow;
 const Tooltip = TooltipPrimitive.Root;
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+export { Tooltip, TooltipArrow, TooltipTrigger, TooltipContent, TooltipProvider };
