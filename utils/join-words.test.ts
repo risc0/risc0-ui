f9d30dd2ffb781609055671b831deab56011334e
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-05-08 11:00:17 -0400

    fix: better joinWords function

--- utils/join-words.test.ts
+++ utils/join-words.test.ts
@@ -16,7 +16,13 @@ describe("joinWords", () => {
   });
 
   it("should return a string with words joined by a space if the input is a string with multiple words", () => {
-    expect(joinWords("HelloWorld")).toBe("Hello World");
+    expect(joinWords("HelloWorldIt'sMe")).toBe("Hello World It's Me");
+    expect(joinWords("Hello_World_It's_Me")).toBe("Hello World It's Me");
+    expect(joinWords("Hello-World-It's-Me")).toBe("Hello World It's Me");
+    expect(joinWords("Hello/World/It's/Me")).toBe("Hello World It's Me");
+    expect(joinWords("Hello\\World\\It's\\Me")).toBe("Hello World It's Me");
+    expect(joinWords("Hello.World.It's.Me")).toBe("Hello World It's Me");
+    expect(joinWords("Hello,World,It's,Me")).toBe("Hello World It's Me");
   });
 
   it("should return the same string if the input is a string with one word", () => {
@@ -25,5 +31,10 @@ describe("joinWords", () => {
 
   it("should return the same string if the passed word is all uppercase", () => {
     expect(joinWords("HELLO")).toBe("HELLO");
+  });
+
+  it("should return the same string if the input is part of the DONT_FORMAT_THESE_WORDS array", () => {
+    expect(joinWords("insn_cycles")).toBe("INSN cycles");
+    expect(joinWords("Linux-nvidia_rtx_a5000")).toBe("Linux NVIDIA RTX a5000");
   });
 });
--- utils/join-words.ts
+++ utils/join-words.ts
@@ -1,9 +1,27 @@
-import { join, words } from "string-ts";
+import { join, toLowerCase } from "string-ts";
+
+const DONT_FORMAT_THESE_WORDS = ["INSN", "ID", "RISC", "RTX", "NVIDIA", "CPU"];
+const DONT_FORMAT_THESE_WORDS_LOWERCASE = DONT_FORMAT_THESE_WORDS.map(toLowerCase);
 
 export function joinWords(str: string): string {
   if (!str) {
     return "";
   }
 
-  return join(words(str), " ");
+  // Insert a space before each uppercase letter that is not at the start of the string or after another uppercase letter
+  const formattedString = str.replace(/(?<=[a-z])(?=[A-Z])/g, " ");
+  const wordsList = formattedString.split(/[\s-_\/\\.,]/); // Remove spaces, hyphens, underscores, slashes, backslashes, dots, and commas
+
+  const formattedWords = wordsList.map((word) => {
+    const lowerCaseWord = toLowerCase(word);
+    const index = DONT_FORMAT_THESE_WORDS_LOWERCASE.indexOf(lowerCaseWord);
+
+    if (index !== -1) {
+      return DONT_FORMAT_THESE_WORDS[index] ?? "";
+    }
+
+    return word;
+  });
+
+  return join(formattedWords, " ");
 }
