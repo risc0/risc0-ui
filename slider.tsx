406aa02ecfa42dd397ae0f8420e1b250b9002e41
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-07-12 09:13:23 -0400

    fix: make slider grab cursor

--- slider.tsx
+++ slider.tsx
@@ -16,13 +16,13 @@ const Slider = forwardRef<
   >
     <SliderPrimitive.Track
       data-testid="slider-track"
-      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
+      className="relative h-1.5 w-full grow cursor-pointer overflow-hidden rounded-full bg-primary/20"
     >
       <SliderPrimitive.Range data-testid="slider-range" className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       data-testid="slider-thumb"
-      className="block size-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+      className="block size-4 cursor-grab rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring active:cursor-grabbing disabled:pointer-events-none disabled:opacity-50"
     />
   </SliderPrimitive.Root>
 ));
