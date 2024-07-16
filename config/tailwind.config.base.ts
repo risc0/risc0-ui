56be2f2ebc8b4b0105855ca6d7b645813fb56990
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-07-16 16:42:57 -0400

    feat: more animations

--- config/tailwind.config.base.ts
+++ config/tailwind.config.base.ts
@@ -77,6 +77,18 @@ const config = {
           from: { opacity: "0", transform: "translateY(6px)" },
           to: { opacity: "1", transform: "translateY(0)" },
         },
+        "fade-in-down": {
+          from: { opacity: "0", transform: "translateY(-6px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-in-left": {
+          from: { opacity: "0", transform: "translateX(6px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        "fade-in-right": {
+          from: { opacity: "0", transform: "translateX(-6px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
         "fade-in": {
           from: { opacity: "0" },
           to: { opacity: "1" },
@@ -93,6 +105,9 @@ const config = {
       animation: {
         "fade-in": "fade-in 1s ease-in-out",
         "fade-in-up": "fade-in-up 1s ease-in-out",
+        "fade-in-down": "fade-in-down 1s ease-in-out",
+        "fade-in-left": "fade-in-left 1s ease-in-out",
+        "fade-in-right": "fade-in-right 1s ease-in-out",
         "head-shake": "head-shake 1s ease-in-out",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
