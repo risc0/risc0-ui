6e129440b38d535dcdae2b400073a551285d8912
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-08-19 13:38:37 -0400

    fix: better ring offset

--- button.tsx
+++ button.tsx
@@ -5,7 +5,7 @@ import { type ButtonHTMLAttributes, type ReactElement, cloneElement, forwardRef 
 import { cn } from "./cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-bold text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-bold text-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
