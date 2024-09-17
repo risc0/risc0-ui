5f03964defdb8e9281ede2f3b22781543791f945
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-17 11:43:09 -0400

    fix: loader pass custom src

--- loader.tsx
+++ loader.tsx
@@ -1,7 +1,10 @@
-export function Loader({ loadingText = "Loading…" }: { loadingText?: string }) {
+export function Loader({
+  loadingSrc = "/loading.gif",
+  loadingText = "Loading…",
+}: { loadingText?: string; loadingSrc?: string }) {
   return (
     <div className="mx-auto flex animate-fade-in-up flex-col items-center justify-center gap-3">
-      <img height={120} width={128} src="/loading.gif" alt="Loading" />
+      <img height={120} width={128} src={loadingSrc} alt="Loading" />
       <p className="text-sm">{loadingText}</p>
     </div>
   );
