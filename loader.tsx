e50086177e628250444381a8027a0189d9c023b0
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-17 10:52:43 -0400

    fix: remove next image

--- loader.tsx
+++ loader.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export function Loader({ loadingText = "Loading…" }: { loadingText?: string }) {
   return (
     <div className="mx-auto flex animate-fade-in-up flex-col items-center justify-center gap-3">
-      <Image unoptimized height={120} width={128} src="/loading.gif" alt="Loading" />
+      <img height={120} width={128} src="/loading.gif" alt="Loading" />
       <p className="text-sm">{loadingText}</p>
     </div>
   );
