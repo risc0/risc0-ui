664d75eb6528370ac66e530186cbaaeee2003aa7
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-11-12 22:13:44 -0500

    fix: tomerged

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,18 @@ import { toMerged } from "es-toolkit";
 import type { Config } from "tailwindcss";
 import tailwindConfig from "./config/tailwind.config.base";
 
-const config = toMerged(tailwindConfig, {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-europa-sans)", "system-ui"],
-        mono: ["var(--font-jetbrains-mono)"],
+const config = toMerged(
+  {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ["var(--font-europa-sans)", "system-ui"],
+          mono: ["var(--font-jetbrains-mono)"],
+        },
       },
     },
   },
-}) satisfies Config;
+  tailwindConfig,
+) satisfies Config;
 
 export default config;
