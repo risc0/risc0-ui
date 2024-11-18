df8042629a3b9b1b7c7e7092dfe50b2493781ad6
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-11-18 08:50:30 -0500

    fix:: deepemerge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
 /* c8 ignore start */
-import { toMerged } from "es-toolkit";
+import deepmerge from "deepmerge";
 import type { Config } from "tailwindcss";
 import tailwindConfig from "./config/tailwind.config.base";
 
-const config = toMerged(
-  {
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ["var(--font-europa-sans)", "system-ui"],
-          mono: ["var(--font-jetbrains-mono)"],
-        },
+const config = deepmerge(tailwindConfig, {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-europa-sans)", "system-ui"],
+        mono: ["var(--font-jetbrains-mono)"],
       },
     },
   },
-  tailwindConfig,
-) satisfies Config;
+}) satisfies Config;
 
 export default config;
